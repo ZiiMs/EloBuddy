451bf0f92fcadb45c4005aef0fbe31d744d29bcb
ZiiMs <alextede8899@gmail.com>
2015-10-08 12:47:58 -0400

    Update LaneClear.cs

--- ZiiM LuLu/Modes/LaneClear.cs
+++ ZiiM LuLu/Modes/LaneClear.cs
@@ -1,4 +1,5 @@
 ﻿using EloBuddy.SDK;
+﻿using Settings = ZiiM.LuLu.Config.LaneClear;
 namespace ZiiM.LuLu.Modes
 {
     public sealed class LaneClear : ModeBase
@@ -11,6 +12,35 @@ namespace ZiiM.LuLu.Modes
 
         public override void Execute()
         {
+            if (Player.Instance.ManaPercent < Settings.Mana)
+            {
+                return;
+            }
+
+            // Minions around
+            var minionsnear = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.ServerPosition, Q.Range);
+            if (minionsnear.Count == 0)
+            {
+                return;
+            }
+            foreach (var minion in EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.ServerPosition, Q.Range))
+            {
+                if (Settings.UseQ && Q.IsReady())
+                {
+                    {
+                        Q.Cast(minion);
+                    }
+                }
+            }
+            foreach (var minion in EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.ServerPosition, E.Range))
+            {
+                if (Settings.UseE && E.IsReady())
+                {
+                    {
+                        E.Cast(minion);
+                    }
+                }
+            }
         }
     }
 }

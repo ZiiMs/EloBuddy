924298d410d3c3d5d2fe8e07bbfb4d4895458393
ZiiMs <alextede8899@gmail.com>
2015-10-16 16:54:27 -0400

    Adding Ekko

---
+++ ZiiM Ekko/ZiiM Ekko/SpellManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using SharpDX;
+
+namespace ZiiM.Ekko
+{
+    public static class SpellManager
+    {
+        // You will need to edit the types of spells you have for each champ as they
+        // don't have the same type for each champ, for example Xerath Q is chargeable,
+        // right now it's  set to Active.
+        public static Spell.Skillshot Q { get; private set; }
+        public static Spell.Skillshot W { get; private set; }
+        public static Spell.Skillshot E { get; private set; }
+        public static Spell.Active R { get; private set; }
+
+        public static List<Spell.SpellBase> AllSpells { get; private set; }
+
+        public static Dictionary<SpellSlot, Color> ColorTranslation { get; private set; }
+
+        static SpellManager()
+        {
+            // Initialize spells
+            Q = new Spell.Skillshot(SpellSlot.Q, 950, SkillShotType.Linear, 250, 1650, 60);
+            W = new Spell.Skillshot(SpellSlot.W, 1600, SkillShotType.Circular, 4000, null, 375);
+            E = new Spell.Skillshot(SpellSlot.E, 500, SkillShotType.Linear, 250, null, 60);
+            R = new Spell.Active(SpellSlot.R, 1600);
+
+            AllSpells = new List<Spell.SpellBase>(new Spell.SpellBase[] { Q, W, E });
+            ColorTranslation = new Dictionary<SpellSlot, Color>
+            {
+                { SpellSlot.Q, Color.Blue.ToArgb(150) },
+                { SpellSlot.W, Color.Purple.ToArgb(150) },
+                { SpellSlot.E, Color.Green.ToArgb(150) }
+            };
+        }
+
+        public static void Initialize()
+        {
+        }
+        private static Color ToArgb(this Color color, byte a)
+        {
+            return new ColorBGRA(color.R, color.G, color.B, a);
+        }
+
+        public static Color GetColor(this Spell.SpellBase spell)
+        {
+            return ColorTranslation.ContainsKey(spell.Slot) ? ColorTranslation[spell.Slot] : Color.Wheat;
+        }
+    }
+}
